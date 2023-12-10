bc68feef61fd72787ebe569a605fa42804bfa56a
Bound Finance <140350394+BoundFinance@users.noreply.github.com>
2023-12-10 16:11:50 +0000

    Update MakerDao.js

--- src/components/MakerDao.js
+++ src/components/MakerDao.js
@@ -7,9 +7,6 @@ import { Link } from "react-router-dom";
 import { BiSolidChevronsRight } from "react-icons/bi";
 import { useAccount, useChainId, useContractRead, useContractEvent } from "wagmi";
 import { ethers } from 'ethers';
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_ALCHEMYHTTPLINK));
-
 import "./Style/MakerDao.css";
 
 // Import contract addresses and ABIs
@@ -17,7 +14,8 @@ import { bcktoeUSD, BCKGovemissions,  eUSD } from '../contract';
 const bckgovemissionsAbi = require('../contract/BCKgovemissions.json'); // Assuming this ABI works for both eUSD and BCK
 const eusdtobckAbi = require('../contract/lsdfitobck.json'); 
 const bckEthAbi = require('../contract/bckEth.json');
-
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_ALCHEMYHTTPLINK));
 
 export const MakerDao = () => {
   const { address } = useAccount();
