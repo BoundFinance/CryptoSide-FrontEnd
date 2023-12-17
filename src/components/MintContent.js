31001d335182551632961747eca81ff7cf61969c
Bound Finance <140350394+BoundFinance@users.noreply.github.com>
2023-12-17 18:48:20 +0000

    Update MintContent.js

--- src/components/MintContent.js
+++ src/components/MintContent.js
@@ -7,26 +7,25 @@ export default function MintContent() {
       <p className="text-32 font-mont font-bold mb-8">
         <p className="step-1">STEP I :</p>
         <div className="line-straight-bck"></div>
-        <p className="mining">Mint BCK</p>
+        <p className="mining">DEPOSIT YIELD BEARING STABLETOKEN</p>
       </p>
       <div className="list-disc text-15 font-mont space-y-7 mt-5 ">
         <div className="d-flex">
           <BiSolidChevronsRight className="mt-1 me-3" />
-          <p className="text-white-100">Mint BCKETH in the BCKETH tab.</p>
+          <p className="text-white-100">For the testnet, purchase some test yield bearing token (eUSD)</p>
         </div>
         <div className="d-flex">
           <BiSolidChevronsRight className="mt-1 me-3" />
-          <p> Lock BCKETH in the CDP page; it converts to BETH.</p>
+          <p>Then deposit your eUSD, This will Allow you to earn more cashback spending Limit</p>
         </div>
         <div className="d-flex">
           <BiSolidChevronsRight className="mt-1 me-3" />
-          <p>Mint BCK, maintaining safe collateral limits.</p>
+          <p>Whilst your eUSD is generating yields, you can mint $BCK 1:1 and spend this across other protocols. This is 0% interest, and $0 borrowing fees</p>
         </div>
         <div className="d-flex">
           <BiSolidChevronsRight className="mt-1 me-3" />
           <p>
-            Adjust BCK with "Pay Back BCK" or take out BCKETH. Mind the
-            collateral ratio.
+            There is no liquidation's, this is because $BCK is a 1:1 mirror of eUSD, and other yield bearing stablecoins. You can use BCK as a normal stablecoin and spend it instead of your yield bearing stablecoins.
           </p>
         </div>
         <div className="d-flex">
@@ -37,9 +36,8 @@ export default function MintContent() {
           <BiSolidChevronsRight className="mt-1 me-3 arrorw" />
           <p>
             {" "}
-            Enjoy no post-loan interest. Earn ETH periodically and withdraw in
-            the Collateral Interest tab.
-          </p>
+            Enjoy no post-loan interest. Earn your yields from your yield bearing stablecoin  as normal.
+            </p>
         </div>
       </div>
     </div>
