f4c22164eeaad5aca29825fba1e7a06ea8f01509
Bound Finance <140350394+BoundFinance@users.noreply.github.com>
2024-01-07 12:32:39 +0000

    Update index.js

--- src/contract/index.js
+++ src/contract/index.js
@@ -7,7 +7,7 @@ const bcktoeUSD = "0xd9823557f1f8992d4764Eb77eCe34cCac8aC824c"; //eusdTobck
 const eUSD =  "0x2410bDEB5D5302aB41Bc3c975A222dA99aDC7b91";  //stabletoken
 const STAKING_ADDRESS = '0xe8EaC117171660f0E45EbC7410ba6531B7d39594';  //BCKsavin
 const esbckgov = '0xD8177347eB9FB3f2CBABc74b808B63ecef6729f3'; //esBCKGOV
-const buyeusd = "0x39DDfB71DC88BDB87D6800D389d9Cf266cce282D"; //buyEusd
+const buyeusd = "0xD299e6560464AfC7b979D6849e7ac2188Db9d65F"; //buyEusd
 const buybckgov = "0x79ea5EF48332A4bE421bd66E1694Ff1DA905eA62"; //buyBCKGOV
 const esbckgovtobckgov = '0xE4C29ea07f640222ae2E7CBA706b268773fb6958'; //vest
 //
